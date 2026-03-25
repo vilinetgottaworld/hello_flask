237352fc703c7c93efa8df32cbd7e7724ba4c749
vilinet <user@ai-04.localdomain>
2026-03-25 11:08:56 +0900

    나만의 프로필 api(json 반환)

--- app.py
+++ app.py
@@ -6,8 +6,14 @@ app = Flask(__name__)
 
 # '/' 주소로 접속하면 실행되는 함수
 @app.route('/')
-def home():
-    return "Hello, World!"
+def my_profile():
+     # 파이썬 딕셔너리를 반환하면 Flask가 알아서 웹 표준인 JSON으로 변환해줍니다!
+     return {
+          "name": "홍길동",
+          "role": "초보 서버 개발자",
+          "status": "로컬 환경 통제 완료!",
+          "skills": ["Ubuntu","VS Code","Python","Flask"]
+     }
 
 # 이 파일을 직접 실행했을 때만 서버 실행
 if __name__ == '__main__':
