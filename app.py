3c8718f67f239f5ff904080812d1c7a13d0abbe5
vilinet <user@ai-04.localdomain>
2026-04-01 09:40:01 +0900

    데이터를 HTML 화면에 표시

--- app.py
+++ app.py
@@ -4,13 +4,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    #여기에 내 데이터를 만든다!
     my_profile= {
-        "name": "홍길동",
-        "age": 20
+        "name": "나윤준",
+        "age": 19,
+        "school": "종로산업정보학교",
+        "hobby": "직곡",
+        "email": "nayoonjun09@gmail.com",
+        "phone": "010-7608-2737",
+        "dream": "개발자"
     }
 
-    return render_template('index.html')
+    return render_template('index.html', data=my_profile)
 
 if __name__ == '__main__':
     app.run(debug=True)
