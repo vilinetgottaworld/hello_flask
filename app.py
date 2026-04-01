19824790e1708219accce1a043dd5a95af772510
vilinet <user@ai-04.localdomain>
2026-04-01 09:07:43 +0900

    HTML 파일 보내기

--- app.py
+++ app.py
@@ -4,7 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    #이제는 JSON이 아니라 HTML 파일을 보냅니다.
+    #여기에 내 데이터를 만든다!
+    my_profile= {
+        "name": "홍길동",
+        "age": 20
+    }
+
     return render_template('index.html')
 
 if __name__ == '__main__':
