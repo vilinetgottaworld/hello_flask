a76a16a203e2c7393ae74946129473298854bf63
vilinet <user@ai-04.localdomain>
2026-04-21 11:28:37 +0900

    삭제 기능 및 확장 구현

--- app.py
+++ app.py
@@ -1,26 +1,43 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
+messages = []
 
 @app.route('/')
 def home():
-    skills = [
-        "C", "C++", "C#", "[BACKEND] Java", "Kotlin",
-        "JavaScript", "TypeScript",
-        "[BACKEND] Python", "Rust", "Go",
-        "[BACKEND] Ruby", "[BACKEND] PHP",
-        "HTML", "CSS", "Markdown",
-        "[DB] SQL", "R",
-        "Bash", "Shell",
-        "Dart", "Swift"
-    ]
-    return render_template('index.html', data=skills)
+    skills = ["Python", "HTML", "SQL", "Bash"]
+    return render_template('index.html', data=skills, messages=messages)
 
 @app.route('/send', methods=['POST'])
 def send():
     skill = request.form.get('skill')
     level = request.form.get('level')
-    return skill + " : " + level
+    status = request.form.get('status')
+    messages.append(skill + " / " + level + " / " + status)
+    return redirect('/')
+
+@app.route('/delete', methods=['POST'])
+def delete():
+    index = int(request.form['index'])
+    messages.pop(index)
+    return redirect('/')
+
+@app.route('/delete_all', methods=['POST'])
+def delete_all():
+    messages.clear()
+    return redirect('/')
+
+@app.route('/delete_selected', methods=['POST'])
+def delete_selected():
+    indexes = request.form.getlist('delete_indexes')
+
+    indexes = [int(index) for index in indexes]
+    indexes.sort(reverse=True)
+
+    for index in indexes:
+        messages.pop(index)
+
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run(debug=True)
